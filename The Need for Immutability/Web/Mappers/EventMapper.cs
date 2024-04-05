0e0df5b5e4b57027df9c93db76d8e7fac406039a
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-05 12:00:04 +0200

    ✨Module 2: Second part

---
+++ The Need for Immutability/Web/Mappers/EventMapper.cs
@@ -0,0 +1,56 @@
+﻿using Shared.Dtos;
+using Web.ViewModels;
+
+namespace Web.Mappers
+{
+    public class EventMapper
+    {
+        public EventViewModel ConvertDtoToViewModel(EventDto dto)
+        {
+            return new EventViewModel
+            {
+                Id = dto.Id,
+                Date = dto.Date,
+                Name = dto.Name,
+                EventType = dto.EventType,
+                Venue = dto.Venue,
+                Capacity = dto.Capacity,
+                Sold = dto.Sold
+            };
+        }
+
+        public void CopyBaseProperties(EventViewModel source, EventViewModel destination)
+        {
+            destination.Date = source.Date;
+            destination.Name = source.Name;
+            destination.EventType = source.EventType;
+            destination.Venue = source.Venue;
+            destination.Capacity = source.Capacity;
+            destination.Sold = source.Sold;
+        }
+
+        public ConferenceDto ConvertConferenceViewModelToDto(ConferenceViewModel viewModel)
+        {
+            return new(viewModel.Id, viewModel.Date, viewModel.Name, viewModel.EventType, viewModel.Venue,
+                viewModel.Capacity, viewModel.Sold, viewModel.BadgeCosts, viewModel.CateringCosts);
+        }
+
+        public MultiDayConferenceDto ConvertMultiDayConferenceViewModelToDto(MultiDayConferenceViewModel viewModel)
+        {
+            return new(viewModel.Id, viewModel.Date, viewModel.Name, viewModel.EventType,
+                viewModel.Venue, viewModel.Capacity, viewModel.Sold, viewModel.BadgeCosts, viewModel.CateringCosts, viewModel.NumberOfDays);
+        }
+
+        public ConcertDto ConvertConcertViewModelToDto(ConcertViewModel viewModel)
+        {
+            return new(viewModel.Id, viewModel.Date, viewModel.Name, viewModel.EventType, viewModel.Venue,
+                viewModel.Capacity, viewModel.Sold, viewModel.ArtistCosts);
+        }
+
+        public SportsGameDto ConvertSportsGameViewModelToDto(SportsGameViewModel viewModel)
+        {
+            return new(viewModel.Id, viewModel.Date, viewModel.Name, viewModel.EventType, viewModel.Venue,
+                viewModel.Capacity, viewModel.Sold, viewModel.NumberOfPlayers);
+        }
+    }
+}
